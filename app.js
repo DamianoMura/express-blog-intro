7275493a4c9e34b748a7f2fe9a1b3163c11ee624
DamianoMura <mura.damiano14@gmail.com>
2025-07-31 15:52:16 +0200

    [add] data.js that contains our posts inside an array of objects

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 //testing that all works with terminal command aliases we just created in package.json
 //console.log("hello world") //debug
 //importing express
-const express = require('express');
+const express = require("express");
 const app = express();
+//importing array of posts
+const data = require("./data.js");
+//lets test if that worked
+console.log(data);
 //creating the endpoint
 app.get('/',(req,res)=>{
   res.send('welcome to my blog');
 })
+
---
+++ data.js
@@ -0,0 +1,29 @@
+const posts =[ 
+  {
+  'title': "first post",
+  'content' : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia veniam dolores quisquam omnis tempora obcaecati neque saepe facere ut voluptatem placeat quam eveniet ipsum, culpa eligendi voluptas earum ipsam. Natus.",
+  'image' : 'img/imgone.jpg'
+},
+{
+  'title': "second post",
+  'content' : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia veniam dolores quisquam omnis tempora obcaecati neque saepe facere ut voluptatem placeat quam eveniet ipsum, culpa eligendi voluptas earum ipsam. Natus.",
+  'image' : 'img/imgtwo.jpg'
+},
+{
+  'title': "third post",
+  'content' : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia veniam dolores quisquam omnis tempora obcaecati neque saepe facere ut voluptatem placeat quam eveniet ipsum, culpa eligendi voluptas earum ipsam. Natus.",
+  'image' : 'img/imgthree.jpg'
+},
+{
+  'title': "fourth post",
+  'content' : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia veniam dolores quisquam omnis tempora obcaecati neque saepe facere ut voluptatem placeat quam eveniet ipsum, culpa eligendi voluptas earum ipsam. Natus.",
+  'image' : 'img/imgfour.jpg'
+},
+{
+  'title': "fifth post",
+  'content' : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia veniam dolores quisquam omnis tempora obcaecati neque saepe facere ut voluptatem placeat quam eveniet ipsum, culpa eligendi voluptas earum ipsam. Natus.",
+  'image' : 'img/imgfive.jpg'
+}
+];
+
+module.exports=posts;
