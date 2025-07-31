919a4100a49e74c532232d07a62cc11992075331
DamianoMura <mura.damiano14@gmail.com>
2025-07-31 15:35:21 +0200

    [add] created endpoint for landing

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 //testing that all works with terminal command aliases we just created in package.json
 //console.log("hello world") //debug
+//importing express
+const express = require('express');
+const app = express();
+//creating the endpoint
+app.get('/',(req,res)=>{
+  
+})
