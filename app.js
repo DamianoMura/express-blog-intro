99ddf62a1e367629ae352be52da221601a865448
DamianoMura <mura.damiano14@gmail.com>
2025-07-31 15:56:00 +0200

    [add] listening port 3000

--- app.js
+++ app.js
@@ -3,12 +3,20 @@
 //importing express
 const express = require("express");
 const app = express();
-//importing array of posts
-const data = require("./data.js");
-//lets test if that worked
-console.log(data);
+const port = 3000;
 //creating the endpoint
 app.get('/',(req,res)=>{
   res.send('welcome to my blog');
 })
+//creating the endpoint bacheca to show our posts
+app.get('/bacheca',(req,res)=>{
+  //importing array of posts
+const data = require("./data.js");
+//lets test if that worked
+console.log(data);
+  res.json(data)
+})
 
+app.listen(port,()=>{
+  console.log(`example app listening on port ${port}`)
+})
