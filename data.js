e3b55dc6a6578f856a76c88de150670c6d5b3f89
DamianoMura <mura.damiano14@gmail.com>
2025-08-01 09:55:47 +0200

    [upd] corrected object keys

--- data.js
+++ data.js
@@ -1,33 +1,33 @@
 const posts =[ 
   {
-  'title': "first post",
-  'content' : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia veniam dolores quisquam omnis tempora obcaecati neque saepe facere ut voluptatem placeat quam eveniet ipsum, culpa eligendi voluptas earum ipsam. Natus.",
+  title: "first post",
+  content : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia veniam dolores quisquam omnis tempora obcaecati neque saepe facere ut voluptatem placeat quam eveniet ipsum, culpa eligendi voluptas earum ipsam. Natus.",
   'image' : 'img/imgone.jpg',
-  'tags' : ['first', 'Jubilous']
+  tags : ['first', 'Jubilous']
 },
 {
-  'title': "second post",
-  'content' : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia veniam dolores quisquam omnis tempora obcaecati neque saepe facere ut voluptatem placeat quam eveniet ipsum, culpa eligendi voluptas earum ipsam. Natus.",
-  'image' : 'img/imgtwo.jpg',
-  'tags' : ['second', 'Celebration']
+  title: "second post",
+  content : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia veniam dolores quisquam omnis tempora obcaecati neque saepe facere ut voluptatem placeat quam eveniet ipsum, culpa eligendi voluptas earum ipsam. Natus.",
+  image : 'img/imgtwo.jpg',
+  tags : ['second', 'Celebration']
 },
 {
-  'title': "third post",
-  'content' : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia veniam dolores quisquam omnis tempora obcaecati neque saepe facere ut voluptatem placeat quam eveniet ipsum, culpa eligendi voluptas earum ipsam. Natus.",
-  'image' : 'img/imgthree.jpg',
-  'tags' : ['third', 'Happiness']
+  title: "third post",
+  content : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia veniam dolores quisquam omnis tempora obcaecati neque saepe facere ut voluptatem placeat quam eveniet ipsum, culpa eligendi voluptas earum ipsam. Natus.",
+  image : 'img/imgthree.jpg',
+  tags : ['third', 'Happiness']
 },
 {
-  'title': "fourth post",
-  'content' : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia veniam dolores quisquam omnis tempora obcaecati neque saepe facere ut voluptatem placeat quam eveniet ipsum, culpa eligendi voluptas earum ipsam. Natus.",
-  'image' : 'img/imgfour.jpg',
-  'tags' : ['fourth', 'Sun God']
+  title: "fourth post",
+  content : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia veniam dolores quisquam omnis tempora obcaecati neque saepe facere ut voluptatem placeat quam eveniet ipsum, culpa eligendi voluptas earum ipsam. Natus.",
+  image : 'img/imgfour.jpg',
+  tags : ['fourth', 'Sun God']
 },
 {
-  'title': "fifth post",
-  'content' : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia veniam dolores quisquam omnis tempora obcaecati neque saepe facere ut voluptatem placeat quam eveniet ipsum, culpa eligendi voluptas earum ipsam. Natus.",
-  'image' : 'img/imgfive.jpg',
-  'tags' : ['fifth', 'Franco']
+  title: "fifth post",
+  content : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia veniam dolores quisquam omnis tempora obcaecati neque saepe facere ut voluptatem placeat quam eveniet ipsum, culpa eligendi voluptas earum ipsam. Natus.",
+  image : 'img/imgfive.jpg',
+  tags : ['fifth', 'Franco']
 }
 ];
 
